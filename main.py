9c68d6c9b06fd676a1eff4abbab3f994d8fbefa0
PiyushBhavsar22 <piyushbhavsar.you@gmail.com>
2025-02-02 15:04:51 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.datascience import logger
 from src.datascience.pipeline.Data_Ingestion import DataIngestionTrainingPipeline
+from src.datascience.pipeline.Data_validation import DataValidationTrainingPipeline
 
 logger.info("Custom logging")
 
@@ -9,6 +10,16 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.initiate_data_ingestion()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_ingestion = DataValidationTrainingPipeline()
+        data_ingestion.initiate_data_validation()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
